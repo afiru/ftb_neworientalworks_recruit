836f919b9c9e330ffcd20daed5ca7bbb0c652ed6
Miho Kinoshita <afiru.pain@gmail.com>
2022-01-17 15:54:20 +0900

    SPメニュー作成しました

--- wp-content/themes/WebPTemplatebyMarcat/include_files/header/header_sp.php
+++ wp-content/themes/WebPTemplatebyMarcat/include_files/header/header_sp.php
@@ -10,3 +10,134 @@
         </div>
     </div>
 </header>
+<nav class="spHeaderNav">
+    <ul class="spHeaderNavUl">
+        <li class="spHeaderNavLi">
+            <div class="btnSubNavi jsbtnSubNavi">
+                <span class="HeaderNavEnTitle">COMPANY</span>
+                <span class="HeaderNavJaTitle">私たちについて</span>
+                <span class="HeaderNaviconPlus"></span>
+            </div>
+            <ul class="spHeaderNavUlSub spHeaderNavUlSub01">
+                <li class="spHeaderNavLiSub">
+                     <a href="<?php echo get_permalink(2); ?>">
+                        <span class="spHeaderNavLiSubmenu">私たちについて</span>
+                    </a>
+                </li>
+                 <li class="spHeaderNavLiSub">
+                     <a href="<?php echo get_permalink(2); ?>">
+                        <span class="spHeaderNavLiSubmenu">会社情報</span>
+                    </a>
+                </li>
+                 <li class="spHeaderNavLiSub">
+                     <a href="<?php echo get_permalink(2); ?>#group">
+                        <span class="spHeaderNavLiSubmenu">グループ会社内容</span>
+                    </a>
+                </li>
+                 <li class="spHeaderNavLiSub">
+                     <a href="<?php echo get_permalink(2); ?>#outline">
+                        <span class="spHeaderNavLiSubmenu">沿革</span>
+                    </a>
+                </li>
+                 <li class="spHeaderNavLiSub">
+                     <a href="<?php echo get_permalink(2); ?>#message">
+                        <span class="spHeaderNavLiSubmenu">代表挨拶</span>
+                    </a>
+                </li>
+            </ul>           
+        </li>
+        
+         <li class="spHeaderNavLi">
+            <div class="btnSubNavi jsbtnSubNavi">
+                <span class="HeaderNavEnTitle">BUSINESS</span>
+                <span class="HeaderNavJaTitle">事業内容</span>
+                <span class="HeaderNaviconPlus"></span>
+            </div>
+            <ul class="spHeaderNavUlSub spHeaderNavUlSub01">
+                <li class="spHeaderNavLiSub">
+                     <a href="<a href="<?php echo get_permalink(30); ?>">
+                        <span class="spHeaderNavLiSubmenu">事業内容</span>
+                    </a>
+                </li>
+                 <li class="spHeaderNavLiSub">
+                     <a href="<a href="<?php echo get_permalink(6); ?>">
+                        <span class="spHeaderNavLiSubmenu">アミューズメント事業</span>
+                    </a>
+                </li>
+                 <li class="spHeaderNavLiSub">
+                     <a href="<a href="<?php echo get_permalink(9); ?>">
+                        <span class="spHeaderNavLiSubmenu">飲食事業</span>
+                    </a>
+                </li>
+                 <li class="spHeaderNavLiSub">
+                     <a href="<a href="<?php echo get_permalink(2); ?>">
+                        <span class="spHeaderNavLiSubmenu">不動産事業</span>
+                    </a>
+                </li>
+            </ul>           
+        </li>
+        
+         <li class="spHeaderNavLi">
+            <div class="btnSubNavi jsbtnSubNavi">
+                <span class="HeaderNavEnTitle">RECRUIT</span>
+                <span class="HeaderNavJaTitle">採用情報</span>
+                <span class="HeaderNaviconPlus"></span>
+            </div>
+            <ul class="spHeaderNavUlSub spHeaderNavUlSub01">
+                <li class="spHeaderNavLiSub">
+                     <a href="<a href="<?php echo get_permalink(31); ?>">
+                        <span class="spHeaderNavLiSubmenu">採用情報</span>
+                    </a>
+                </li>
+                 <li class="spHeaderNavLiSub">
+                     <a href="<a href="<?php echo get_permalink(57); ?>">
+                        <span class="spHeaderNavLiSubmenu">スタッフインタビュー</span>
+                    </a>
+                </li>
+                 <li class="spHeaderNavLiSub">
+                     <a href="<a href="<?php echo get_permalink(35); ?>">
+                        <span class="spHeaderNavLiSubmenu">募集要項</span>
+                    </a>
+                </li>
+            </ul>           
+        </li>
+        
+         <li class="spHeaderNavLi">
+            <div class="btnSubNavi jsbtnSubNavi">
+                <span class="HeaderNavEnTitle">INFORMATION</span>
+                <span class="HeaderNavJaTitle">お知らせ・その他</span>
+                <span class="HeaderNaviconPlus"></span>
+            </div>
+            <ul class="spHeaderNavUlSub spHeaderNavUlSub01">
+                 <li class="spHeaderNavLiSub">
+                     <a href="<a href="<?php echo get_permalink(1); ?>">
+                        <span class="spHeaderNavLiSubmenu">お知らせ</span>
+                    </a>
+                </li>
+                 <li class="spHeaderNavLiSub">
+                     <a href="<a href="<?php echo get_permalink(2); ?>">
+                        <span class="spHeaderNavLiSubmenu">グループ会社内容</span>
+                    </a>
+                </li>
+                 <li class="spHeaderNavLiSub">
+                     <a href="<a href="<?php echo get_permalink(3); ?>">
+                        <span class="spHeaderNavLiSubmenu">個人情報保護方針</span>
+                    </a>
+                </li>
+                <li class="spHeaderNavLiSub">
+                     <a href="<a href="<?php echo get_permalink(49); ?>">
+                        <span class="spHeaderNavLiSubmenu">サイトマップ</span>
+                    </a>
+                </li>
+                <li class="spHeaderNavLiSub">
+                     <a href="<a href="<?php echo get_permalink(43); ?>">
+                        <span class="spHeaderNavLiSubmenu">お問い合わせ</span>
+                    </a>
+                </li>
+            </ul>           
+        </li>
+    </ul>
+    <div class="spHeaderNavContactBtn">
+        <a href="spHeaderNavContactBtnLink" href="<a href="<?php echo get_permalink(43); ?>"></a>
+    </div>
+</nav>
