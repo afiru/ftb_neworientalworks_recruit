9fd812515a00fcc634737f7daad4cdcb40835f32
Naoki Soga <n.soga@sunplacom>
2022-01-17 14:53:57 +0900

    追加

--- wp-content/themes/WebPTemplatebyMarcat/include_files/index/03_Company.php
+++ wp-content/themes/WebPTemplatebyMarcat/include_files/index/03_Company.php
@@ -1,8 +1,35 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<div class="ovhidden bg_fff indexCompany">
+    <div class="wapper po_re indexCompanyWap">
+        <section class="titleIndexCompany">
+            <h1 class="h1TitleIndexCompany">
+                <img loading="lazy" src="<?php echo get_bloginfo('template_url'); ?>/img/index/h1TitleIndexCompany.svg" alt="COMPANY" width="48.67" height="187" />
+            </h1>
+            <p class="cl_006FDB txtTitleIndexCompany">私たちについて</p>            
+            <nav class="navIndexCompany">
+                <ul class="ulIndexCompany">
+                    <li class="liIndexCompany">
+                        <a href="<?php echo get_the_permalink(2); ?>#message">・代表メッセージ</a>
+                    </li>
+                    <li class="liIndexCompany">
+                        <a href="<?php echo get_the_permalink(2); ?>#outline">・沿革</a>
+                    </li>
+                    <li class="liIndexCompany">
+                        <a href="<?php echo get_the_permalink(2); ?>#group">・グループ会社内容</a>
+                    </li>
+                </ul>
+                <div class="btnIndexCompanyWap">
+                    <a class="btnIndexCompany" href="<?php echo get_the_permalink(2); ?>">
+                        <span class="arrowBtnIndexCompany">詳しくはこちら</span>
+                    </a>
+                </div>
+            </nav>
+        </section>
+        
+        <div class="bgindexCompany">
+            <!--
+                pc:../img/index/bgindexCompanyPc.png
+                Sp:../img/index/bgindexCompanySp.png
+            -->
+        </div>
+    </div>
+</div>
--- wp-content/themes/WebPTemplatebyMarcat/include_files/index/05_Recruit.php
+++ wp-content/themes/WebPTemplatebyMarcat/include_files/index/05_Recruit.php
@@ -1,8 +1,42 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<div class="bg_fff ovhidden indexRecruit">
+    <div class="wapper po_re indexRecruitWap">
+        <div class="display_flex_stretch display_row titleIndexRecruit">
+            <h1 class="h1TitleIndexRecruit">
+                <img loading="lazy" src="<?php echo get_bloginfo('template_url'); ?>/img/index/h1TitleIndexRecruit.svg" alt="リクルート" width="365.33" height="48.67" />
+            </h1>
+            <p class="cl_006FDB txtTitleIndexCompany">採用情報</p> 
+        </div>
+        <div class="po_re navIndexRecruit">            
+            <div class="navIndexRecruitWap">
+                <div class="bgIndexRecruit"></div>
+                <ul class="display_flex_stretch ulIndexRecruit">
+                    <li class="liIndexRecruit liIndexRecruit01">
+                        <a class="btnIndexRecruit" href="<?php echo get_the_permalink(31); ?>#merit" target="_blnak">
+                            <img class="nomal" loading="lazy" src="<?php echo get_bloginfo('template_url'); ?>/img/index/btnIndexRecruit01off.svg" alt="3つの魅力" width="216" height="216" />
+                            <img class="hover" loading="lazy" src="<?php echo get_bloginfo('template_url'); ?>/img/index/btnIndexRecruit01on.svg" alt="3つの魅力" width="216" height="216" />
+                        </a>
+                    </li>
+                    <li class="liIndexRecruit liIndexRecruit02">
+                        <a class="btnIndexRecruit" href="<?php echo get_the_permalink(57); ?>" target="_blnak">
+                            <img class="nomal" loading="lazy" src="<?php echo get_bloginfo('template_url'); ?>/img/index/btnIndexRecruit02off.svg" alt="スタッフインタビュー" width="216" height="216" />
+                            <img class="hover" loading="lazy" src="<?php echo get_bloginfo('template_url'); ?>/img/index/btnIndexRecruit02on.svg" alt="スタッフインタビュー" width="216" height="216" />
+                        </a>
+                    </li>
+                    <li class="liIndexRecruit liIndexRecruit03">
+                        <a class="btnIndexRecruit" href="<?php echo get_the_permalink(30); ?>" target="_blnak">
+                            <img class="nomal" loading="lazy" src="<?php echo get_bloginfo('template_url'); ?>/img/index/btnIndexRecruit03off.svg" alt="募集要項" width="216" height="216" />
+                            <img class="hover" loading="lazy" src="<?php echo get_bloginfo('template_url'); ?>/img/index/btnIndexRecruit03on.svg" alt="募集要項" width="216" height="216" />
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="bgPhotoIndexRecruit">
+                <picture>
+                    <source media="(min-width: 768px)" srcset="<?php echo get_bloginfo('template_url'); ?>/img/index/bgPhotoIndexRecruitPc.png"><!--PC画像-->
+                    <source media="(max-width: 768px)" srcset="<?php echo get_bloginfo('template_url'); ?>/img/index/bgPhotoIndexRecruitSp.png"><!--SP画像-->
+                    <img loading="lazy" src="<?php echo get_bloginfo('template_url'); ?>/img/index/bgPhotoIndexRecruitPc.png" alt="" width="840" height="547" />
+                </picture>
+            </div>
+        </div>
+    </div>
+</div>
