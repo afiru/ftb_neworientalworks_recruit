dad2af0b22e23785116570b568202578b664f76e
Naoki Soga <n.soga@sunplacom>
2022-01-18 11:20:20 +0900

    ---

--- wp-content/themes/WebPTemplatebyMarcat/tempPageService.php
+++ wp-content/themes/WebPTemplatebyMarcat/tempPageService.php
@@ -10,12 +10,13 @@
 <aside class="wapper breadCrumbList"><?php if(function_exists('bcn_display')) { bcn_display(); }?></aside>
 <?php if ( have_posts() ) while ( have_posts() ) : the_post();  ?>
 <main class="<?php echo $post->post_name; ?>Main">
-        <?php get_template_part('include_files/page/company/02_message'); ?>
-        <?php get_template_part('include_files/page/company/03_company'); ?>
-        <?php get_template_part('include_files/page/company/04_group'); ?>
-        <?php get_template_part('include_files/page/company/05_social'); ?>
-        <?php get_template_part('include_files/page/company/06_outline'); ?>
-        <?php get_template_part('include_files/page/company/07_btmNav'); ?>
+    <?php get_template_part('include_files/page/service/02_newOriental'); ?>
+    <?php get_template_part('include_files/page/service/03_Opasu'); ?>
+    <?php get_template_part('include_files/page/service/04_Amyuz'); ?>
+    <?php get_template_part('include_files/page/service/05_Shintoyo'); ?>
+    <?php get_template_part('include_files/page/service/06_ziOrient'); ?>
+    <?php get_template_part('include_files/page/service/07_restaurant'); ?>
+    <?php get_template_part('include_files/page/service/08_realestate'); ?>
 <?php endwhile; // end of the loop. ?>
 </main>
 <?php get_footer(); ?>
