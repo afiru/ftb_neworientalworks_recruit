11ac96b40aaf508819f8568520cb853379f80f70
Naoki Soga <n.soga@sunplacom>
2022-01-18 18:12:34 +0900

    --

--- wp-content/themes/WebPTemplatebyMarcat/include_files/page/recruit/03_merit.php
+++ wp-content/themes/WebPTemplatebyMarcat/include_files/page/recruit/03_merit.php
@@ -1,8 +1,10 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<div id="merit" class="recruitMerit">
+    <h1 class="t_center h1RecruitMerit">ニューオリエンタルワークスの<br class="sp_only">3つの魅力</h1>
+    <div class="bg_FFFFF6 bgRecruitMerit">
+        <div class="wapper display_flex_stretch display_row recruitMeritWap">
+            <div class="po_re recruitMeritCnt">
+                <figure class="iconRecruitMerit"></figure>
+            </div>
+        </div>
+    </div>
+</div>
