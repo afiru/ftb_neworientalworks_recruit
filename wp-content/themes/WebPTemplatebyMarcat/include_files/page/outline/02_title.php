e52646cfa9597509a1786b051e45f4edaa3ab890
Naoki Soga <n.soga@sunplacom>
2022-01-19 17:23:22 +0900

    ----

---
+++ wp-content/themes/WebPTemplatebyMarcat/include_files/page/outline/02_title.php
@@ -0,0 +1,11 @@
+<div class="ServiceCnt outlineCnt">
+    <div class="wapper companySocialWap">
+        <section class="titleCompany">
+            <h2 class="t_center oswald cl_006FDB h2TitleCompany">OUTLINE</h2>
+            <h3 class="t_center cl_006FDB h3TitleCompany">募集要項</h3>
+            <p class="po_re bdTitleCompanyWap">
+                <span class="bdTitleCompany"></span>
+            </p>
+        </section>
+    </div>
+</div>
--- wp-content/themes/WebPTemplatebyMarcat/tempPageOutline.php
+++ wp-content/themes/WebPTemplatebyMarcat/tempPageOutline.php
@@ -1,6 +1,6 @@
 <?php
     /**
-     * Template Name: スタッフ紹介
+     * Template Name: 募集要項
      * Template Post Type: page
      */
 ?>
@@ -10,10 +10,11 @@
 <aside class="wapper breadCrumbList"><?php if(function_exists('bcn_display')) { bcn_display(); }?></aside>
 <?php if ( have_posts() ) while ( have_posts() ) : the_post();  ?>
 <main class="<?php echo $post->post_name; ?>Main">
-    <?php get_template_part('include_files/page/staff/02_title'); ?>
-    <?php get_template_part('include_files/page/staff/03_Loop'); ?>
+    <?php get_template_part('include_files/page/outline/02_title'); ?>
+    <?php get_template_part('include_files/page/outline/03_Loop'); ?>
     <?php get_template_part('include_files/page/recruit/04_entry'); ?>
     <?php get_template_part('include_files/page/recruit/06_btmNav'); ?>
-<?php endwhile; // end of the loop. ?>
+
 </main>
+<?php endwhile; // end of the loop. ?>
 <?php get_footer(); ?>
