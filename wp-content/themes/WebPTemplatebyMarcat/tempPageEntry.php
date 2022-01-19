c7a5a36e4d2a5ad37805385da32b092626209910
Naoki Soga <n.soga@sunplacom>
2022-01-19 19:35:55 +0900

    ---

--- wp-content/themes/WebPTemplatebyMarcat/tempPageEntry.php
+++ wp-content/themes/WebPTemplatebyMarcat/tempPageEntry.php
@@ -1,6 +1,6 @@
 <?php
     /**
-     * Template Name: 採用情報
+     * Template Name: エントリー
      * Template Post Type: page
      */
 ?>
@@ -10,12 +10,7 @@
 <aside class="wapper breadCrumbList"><?php if(function_exists('bcn_display')) { bcn_display(); }?></aside>
 <?php if ( have_posts() ) while ( have_posts() ) : the_post();  ?>
 <main class="<?php echo $post->post_name; ?>Main">
-    <?php get_template_part('include_files/page/recruit/02_like'); ?>
-    <?php get_template_part('include_files/page/recruit/03_merit'); ?>
-    <?php get_template_part('include_files/page/recruit/04_entry'); ?>
-    <?php get_template_part('include_files/page/recruit/05_staff'); ?>
-    <?php get_template_part('include_files/page/recruit/06_btmNav'); ?>
-
+    <?php the_content(); ?>
 </main>
 <?php endwhile; // end of the loop. ?>
 <?php get_footer(); ?>
---
+++ wp-content/themes/WebPTemplatebyMarcat/固定ページ/エントリー/01_入力.php
@@ -0,0 +1,13 @@
+<div class="wapper contactWap">
+    <figure class="formStep">
+        <picture>
+            <source media="(min-width: 768px)" srcset="<?php echo get_bloginfo('template_url'); ?>/img/formStep01Pc.svg"><!--PC画像-->
+            <source media="(max-width: 768px)" srcset="<?php echo get_bloginfo('template_url'); ?>/img/formStep01Sp.svg"><!--SP画像-->
+            <img loading="lazy" src="<?php echo get_bloginfo('template_url'); ?>/img/formStep01Pc.svg" alt="フォームステップ" width="345" height="88" />
+        </picture>
+    </figure>
+    
+    <div class="formCnt FormContact">
+        [mwform_formkey key="23"]
+    </div>
+</div>
