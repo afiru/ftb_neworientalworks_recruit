80343309cd34d260dc1bbb7b0a611d6d018c2779
Miho Kinoshita <afiru.pain@gmail.com>
2022-01-19 19:33:30 +0900

    --

--- wp-content/themes/WebPTemplatebyMarcat/include_files/page/recruit/03_merit.php
+++ wp-content/themes/WebPTemplatebyMarcat/include_files/page/recruit/03_merit.php
@@ -4,7 +4,7 @@
         <div class="wapper display_flex_stretch display_row recruitMeritWap">
             <div class="po_re recruitMeritCnt recruitMeritCnt01">
                 <figure class="iconRecruitMerit">
-                    <img loading="lazy" src="<?php echo get_bloginfo('template_url'); ?>/img/recruit/iconRecruitMerit01.svg" alt="Good" width="108" height="108" />
+                    <img loading="lazy" src="<?php echo get_bloginfo('template_url'); ?>/img/page/recruit/iconRecruitMerit01.svg" alt="Good" width="108" height="108" />
                 </figure>
                 <section class="secRecruitMeritCnt">
                     <h3 class="t_center h3RecruitMeritCnt">特別賞与でしっかりと<br>従業員の頑張りを評価します！</h3>
@@ -16,7 +16,7 @@
             
             <div class="po_re recruitMeritCnt recruitMeritCnt02">
                 <figure class="iconRecruitMerit">
-                    <img loading="lazy" src="<?php echo get_bloginfo('template_url'); ?>/img/recruit/iconRecruitMerit02.svg" alt="リボン" width="108" height="108" />
+                    <img loading="lazy" src="<?php echo get_bloginfo('template_url'); ?>/img/page/recruit/iconRecruitMerit02.svg" alt="リボン" width="108" height="108" />
                 </figure>
                 <section class="secRecruitMeritCnt">
                     <h3 class="t_center h3RecruitMeritCnt">おしゃれに楽しく<br>働く事が出来ますよ!</h3>
@@ -28,7 +28,7 @@
             
             <div class="po_re recruitMeritCnt recruitMeritCnt03">
                 <figure class="iconRecruitMerit">
-                    <img loading="lazy" src="<?php echo get_bloginfo('template_url'); ?>/img/recruit/iconRecruitMerit03.svg" alt="握手" width="108" height="108" />
+                    <img loading="lazy" src="<?php echo get_bloginfo('template_url'); ?>/img/page/recruit/iconRecruitMerit03.svg" alt="握手" width="108" height="108" />
                 </figure>
                 <section class="secRecruitMeritCnt">
                     <h3 class="t_center h3RecruitMeritCnt">教育・サポート体制が<br>充実しているので安心!</h3>
--- wp-content/themes/WebPTemplatebyMarcat/include_files/page/recruit/04_entry.php
+++ wp-content/themes/WebPTemplatebyMarcat/include_files/page/recruit/04_entry.php
@@ -1,16 +1,21 @@
 <div class="wapper recruitEntryBtnWap">
     <a class="recruitEntryBtn" href="<?php echo get_permalink(35); ?>#employee">
         <picture>
-            <source media="(min-width: 768px)" srcset="<?php echo get_bloginfo('template_url'); ?>/img/recruit/recruitEntryBtn01Pc.png"><!--PC画像-->
-            <source media="(max-width: 768px)" srcset="<?php echo get_bloginfo('template_url'); ?>/img/recruit/recruitEntryBtn01Sp.png"><!--SP画像-->
+            <source media="(min-width: 768px)" srcset="<?php echo get_bloginfo('template_url'); ?>/img/page/recruit/recruitEntryBtn01Pc.png"><!--PC画像-->
+            <source media="(max-width: 768px)" srcset="<?php echo get_bloginfo('template_url'); ?>/img/page/recruit/recruitEntryBtn01Sp.png"><!--SP画像-->
             <img loading="lazy" src="<?php echo get_bloginfo('template_url'); ?>/img/recruit/recruitEntryBtn01Pc.png" alt="正社員募集" width="580" height="195" />
         </picture>
     </a>
     <a class="recruitEntryBtn" href="<?php echo get_permalink(35); ?>#parttime">
         <picture>
-            <source media="(min-width: 768px)" srcset="<?php echo get_bloginfo('template_url'); ?>/img/recruit/recruitEntryBtn02Pc.png"><!--PC画像-->
-            <source media="(max-width: 768px)" srcset="<?php echo get_bloginfo('template_url'); ?>/img/recruit/recruitEntryBtn02Sp.png"><!--SP画像-->
+            <source media="(min-width: 768px)" srcset="<?php echo get_bloginfo('template_url'); ?>/img/page/recruit/recruitEntryBtn02Pc.png"><!--PC画像-->
+            <source media="(max-width: 768px)" srcset="<?php echo get_bloginfo('template_url'); ?>/img/page/recruit/recruitEntryBtn02Sp.png"><!--SP画像-->
             <img loading="lazy" src="<?php echo get_bloginfo('template_url'); ?>/img/recruit/recruitEntryBtn02Pc.png" alt="アルバイト募集" width="580" height="195" />
         </picture>
+    </a>
+</div>
+<div class="wapper recruitEntryBtnWap02">
+    <a class="recruitEntryBtn02Link" href="<?php echo get_permalink(35); ?>#parttime">
+        <span class="recruitEntryArrow02">エントリー</span>
     </a>
 </div>
