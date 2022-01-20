5e0b9ef17fc18f501fbc1f822b772de4c45171de
Naoki Soga <n.soga@sunplacom>
2022-01-20 16:05:36 +0900

    問い合わせ追加

---
+++ wp-content/themes/WebPTemplatebyMarcat/include_files/page/contact/01_Fv.php
@@ -0,0 +1,10 @@
+<div class="pageFv display_flex_center bgCcotnactPageFv">
+    <!--
+    Pc:../img/page/contact/bgCompanyPageFvPc.jpg
+    Sp:../img/page/contact/bgCompanyPageFvSp.jpg
+    -->
+    <section class="titlePageFv">
+        <h1 class="cl_fff t_center h1TitlePageFv">お問い合わせ</h1>
+        <p class="t_center cl_fff txtTitlePageFv">CONTACT</p>
+    </section>
+</div>
--- wp-content/themes/WebPTemplatebyMarcat/固定ページ/エントリー/01_入力.php
+++ wp-content/themes/WebPTemplatebyMarcat/固定ページ/エントリー/01_入力.php
@@ -1,9 +1,9 @@
 <div class="wapper contactWap">
     <figure class="formStep">
         <picture>
-            <source media="(min-width: 768px)" srcset="<?php echo get_bloginfo('template_url'); ?>/img/formStep01Pc.svg"><!--PC画像-->
-            <source media="(max-width: 768px)" srcset="<?php echo get_bloginfo('template_url'); ?>/img/formStep01Sp.svg"><!--SP画像-->
-            <img loading="lazy" src="<?php echo get_bloginfo('template_url'); ?>/img/formStep01Pc.svg" alt="フォームステップ" width="345" height="88" />
+            <source media="(min-width: 768px)" srcset="[theme_img]/formStep01Pc.svg"><!--PC画像-->
+            <source media="(max-width: 768px)" srcset="[theme_img]/formStep01Sp.svg"><!--SP画像-->
+            <img loading="lazy" src="[theme_img]/formStep01Pc.svg" alt="フォームステップ" width="345" height="88" />
         </picture>
     </figure>
     
