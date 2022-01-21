02139facd0c719efbdfc70f3efd90737aeadb6b3
Yuji Morimatsu <morimatsu-k@sunpla.com>
2022-01-21 16:58:16 +0900

    --

--- wp-content/themes/WebPTemplatebyMarcat/固定ページ/エントリー/03_完了.php
+++ wp-content/themes/WebPTemplatebyMarcat/固定ページ/エントリー/03_完了.php
@@ -1,8 +1,8 @@
 <div class="wapper contactWap">
     <figure class="formStep">
         <picture>
-            <source media="(min-width: 768px)" srcset="[theme_img]/formStep01Pc.svg"><!--PC画像-->
-            <source media="(max-width: 768px)" srcset="[theme_img]/formStep01Sp.svg"><!--SP画像-->
+            <source media="(min-width: 768px)" srcset="[theme_img]/formStep03Pc.svg"><!--PC画像-->
+            <source media="(max-width: 768px)" srcset="[theme_img]/formStep03Sp.svg"><!--SP画像-->
             <img loading="lazy" src="[theme_img]/formStep01Pc.svg" alt="フォームステップ" width="345" height="88" />
         </picture>
     </figure>
