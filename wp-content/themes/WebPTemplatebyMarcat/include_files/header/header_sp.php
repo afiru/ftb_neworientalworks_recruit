74a07390d4d55733b8020bdb3e48eb9be5f03f59
Miho Kinoshita <afiru.pain@gmail.com>
2022-01-21 17:13:32 +0900

    カンパニー画像追加

--- wp-content/themes/WebPTemplatebyMarcat/include_files/header/header_sp.php
+++ wp-content/themes/WebPTemplatebyMarcat/include_files/header/header_sp.php
@@ -1,6 +1,6 @@
 <header class="spHeader">
     <div class="wapper display_flex_center spHeaderWap">
-        <a class="buttonPcHeader" href="<?php echo home_url('/'); ?>">
+        <a class="buttonSpHeader" href="<?php echo home_url('/'); ?>">
             <img loading="lazy" src="<?php echo get_bloginfo('template_url'); ?>/img/logoHeaderPc.svg" alt="<?php bloginfo('name'); ?>" width="362" height="46.46" />
         </a> 
         <div class="sp_menu_box_wapper sp_menu_button off">
