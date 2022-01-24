1b7f9eb57b8aca93745ad93a5ae4253174fcc8ae
Naoki Soga <n.soga@sunplacom>
2022-01-24 16:51:41 +0900

    ---

--- wp-content/themes/WebPTemplatebyMarcat/tempPageOutline.php
+++ wp-content/themes/WebPTemplatebyMarcat/tempPageOutline.php
@@ -6,13 +6,12 @@
 ?>
 <?php get_header(); ?>
 <?php remove_filter ('the_content', 'wpautop'); ?>
-<?php get_template_part('include_files/page/recruit/01_Fv'); ?>
+<?php get_template_part('include_files/page/outline/01_Fv'); ?>
 <aside class="wapper breadCrumbList"><?php if(function_exists('bcn_display')) { bcn_display(); }?></aside>
 <?php if ( have_posts() ) while ( have_posts() ) : the_post();  ?>
 <main class="<?php echo $post->post_name; ?>Main">
     <?php get_template_part('include_files/page/outline/02_title'); ?>
     <?php get_template_part('include_files/page/outline/03_Loop'); ?>
-    <?php get_template_part('include_files/page/recruit/04_entry'); ?>
     <?php get_template_part('include_files/page/recruit/06_btmNav'); ?>
 
 </main>
